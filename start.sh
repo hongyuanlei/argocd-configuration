874f6b9589685b9f4517f87cc50d08ecb903c3d7
Yuanlei Hong <ylhong@YuanleideMacBook-Pro.local>
2025-06-09 17:19:36 +0800

    update

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e  # Exit on error
 
+kubectl apply -f infrastructure/k8s/namespace.yaml
+
 # Step 1: Apply Argo CD manifests
-kubectl apply -k infrastructure/argocd-install
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # register the argocd-configuration to argocd
 
 # Step 2: Wait for Argo CD components to be ready
@@ -11,12 +13,17 @@ echo "Waiting for Argo CD to become ready..."
 kubectl wait deployment/argocd-server \
   --namespace argocd \
   --for=condition=Available \
-  --timeout=180s
+  --timeout=300s
+
+kubectl wait deployment/argocd-applicationset-controller \
+  --namespace argocd \
+  --for=condition=Available \
+  --timeout=300s
 
 # Step 3: Continue with other commands
 echo "Argo CD is ready. Running follow-up commands..."
-kubectl apply -f infrastructure/namespaces/namespace.yaml
-kubectl apply -f bootstrap.yaml
+
+kubectl apply -f infrastructure/argocd/bootstrap.yaml
 
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
 
