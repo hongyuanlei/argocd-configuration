f2f3a81b61577d3f5c6a95cb1dd795cbb3e20bdf
Yuanlei Hong <ylhong@YuanleideMacBook-Pro.local>
2025-06-19 17:13:25 +0800

    update

--- install-loki.sh
+++ install-loki.sh
@@ -4,4 +4,16 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 helm upgrade --install loki grafana/loki \
-  --namespace=observability
+  --namespace=observability \
+  --set deploymentMode=SingleBinary \
+  --set singleBinary.replicas=1 \
+  --set read.replicas=0 \
+  --set write.replicas=0 \
+  --set backend.replicas=0 \
+  --set gateway.enabled=false \
+  --set loki.auth_enabled=false \
+  --set loki.storage.type=filesystem \
+  --set loki.storage.filesystem.directory=/data/loki \
+  --set persistence.enabled=true \
+  --set persistence.size=5Gi \
+  --set loki.useTestSchema=true
--- install-prometheus.sh
+++ install-prometheus.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Prometheus stack
-echo "Install prometheus stack"
+echo "Install prometheus, alert-manager and grafana stack"
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install prometheus prometheus-community/kube-prometheus-stack -n observability
